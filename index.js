14f660fc0ce96cdd84df0e6b0478a9c205c2a2a8
Mohd Imran <mohd78696321@gmail.com>
2021-09-27 09:40:52 +0530

    feat: added new questions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var readlineSync = require('readline-sync');
-const chalk = require('chalk');
+var readlineSync = require("readline-sync");
+const chalk = require("chalk");
 
 var userName = readlineSync.question("What is your name? ");
 
@@ -8,36 +8,45 @@ console.log("Welcome " + userName + "! Do You Know Me?");
 var score = 0;
 
 function play(question, answer) {
-    var userAnswer = readlineSync.question(question);
+  var userAnswer = readlineSync.question(question);
 
-    if (userAnswer === answer) {
-        console.log(chalk.green("You are Right!"));
-        score = score + 1;
+  if (userAnswer === answer) {
+    console.log(chalk.green("You are Right!"));
+    score = score + 1;
+  } else {
+    console.log(chalk.red("You are Wrong!"));
+  }
 
-    } else {
-        console.log(chalk.red("You are Wrong!"));
-    }
-
-    console.log(chalk.blue("Score: " + score));
-    console.log("---------");
+  console.log(chalk.blue("Score: " + score));
+  console.log("---------");
 }
 
-var questions = [{
+var questions = [
+  {
     question: "What is my Name? ",
     answer: "Imran",
-},
-{
+  },
+  {
     question: "Where do I live? ",
     answer: "Gorakhpur",
-},
-{
+  },
+  {
     question: "My favourite Movie? ",
     answer: "Harry Potter",
-}];
+  },
+  {
+    question: "I am a Tea or Coffee Person? ",
+    answer: "Coffee",
+  },
+  {
+    question: "I am Graduated In ",
+    answer: "Computer Science",
+  },
+];
 
 for (var i = 0; i < questions.length; i++) {
-    var currentQuestion = questions[i];
-    play(currentQuestion.question, currentQuestion.answer);
+  var currentQuestion = questions[i];
+  play(currentQuestion.question, currentQuestion.answer);
 }
 
 console.log(chalk.bgGreen("Total Score: " + score));
